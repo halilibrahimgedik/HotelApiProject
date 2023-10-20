a0f8feb1b743baa2dc47703fafbc22dbfc36ae50
Halil Gedik <halilgedik4234@gmail.com>
2023-10-20 23:03:31 +0300

    Initial

---
+++ ApiConsume/MyHotel.EntityLayer/Concrete/Service.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHotel.EntityLayer.Concrete
+{
+    public class Service
+    {
+        public int ServiceId { get; set; }
+
+        public string ServiceIcon { get; set; }
+
+        public string Tıtle { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ ApiConsume/MyHotel.EntityLayer/Concrete/Subscribe.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHotel.EntityLayer.Concrete
+{
+    public class Subscribe
+    {
+        public int SubscribeId { get; set; }
+
+        public string Mail { get; set; }
+    }
+}
