0112c966d480723b0f449d18f5f4cc354efdc827
Halil Gedik <halilgedik4234@gmail.com>
2023-11-01 21:47:34 +0300

    Hizmetlerimiz eklendi

--- Frontend/MyHotel.WebUI/ViewComponents/Default/ServiceViewComponent.cs
+++ Frontend/MyHotel.WebUI/ViewComponents/Default/ServiceViewComponent.cs
@@ -1,11 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyHotel.WebUI.DTOs.ServiceDto;
+using Newtonsoft.Json;
+using System.Runtime.InteropServices;
 
 namespace MyHotel.WebUI.ViewComponents.Default
 {
     public class ServiceViewComponent : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory httpClientFactory;
+        public ServiceViewComponent(IHttpClientFactory httpClientFactory)
         {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = httpClientFactory.CreateClient();
+            var response = await client.GetAsync("http://localhost:5120/api/service");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var stringData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ListServiceDto>>(stringData);
+
+                return View(values);
+            }
+
             return View();
         }
     }
