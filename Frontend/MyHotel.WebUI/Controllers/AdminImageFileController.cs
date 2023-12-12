346e09b425d112e516df98c81187603c386212e2
Halil Gedik <halilgedik4234@gmail.com>
2023-12-12 14:49:11 +0300

    Admin ImageFileController oluşturuldu

---
+++ Frontend/MyHotel.WebUI/Controllers/AdminImageFileController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyHotel.WebUI.Controllers
+{
+    public class AdminImageFileController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(IFormFile file)
+        {
+            return View();
+        }
+    }
+}
